f873f24c0f4ed9dfb77d221a48cf7e7014267861
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-30 18:16:22 +0500

    imp

---
+++ lib/pages/checkbox_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CheckboxPage extends StatefulWidget {
+  const CheckboxPage({super.key});
+
+  @override
+  State<CheckboxPage> createState() => _CheckboxPageState();
+}
+
+class _CheckboxPageState extends State<CheckboxPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String? selectedPayment;
   String? selectedGender;
+  String? selectRegion;
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +21,17 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.all(20),
           child: Column(
             children: [
+              Row(
+                children: [
+                  Text(
+                    "Payment Method",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
               RadioListTile<String>(
                 value: "cash",
                 groupValue: selectedPayment,
@@ -45,6 +58,60 @@ class _HomePageState extends State<HomePage> {
                   setState(() {});
                 },
                 title: Text("Humo"),
+              ),
+              CupertinoButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  selectedPayment = null;
+                  setState(() {});
+                },
+                child: Text("Unselect"),
+              ),
+              Divider(),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(
+                    "Select Region",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+              RadioListTile(
+                value: "tashkent",
+                activeColor: Colors.red,
+                selected: selectRegion == "tashkent",
+                groupValue: selectRegion,
+                onChanged: (value) {
+                  selectRegion = value;
+                  setState(() {});
+                },
+                title: Text("Tashkent"),
+              ),
+              RadioListTile(
+                value: "samarkand",
+                activeColor: Colors.red,
+                selected: selectRegion == "samarkand",
+                groupValue: selectRegion,
+                onChanged: (value) {
+                  selectRegion = value;
+                  setState(() {});
+                },
+                title: Text("Samarkand"),
+              ),
+              RadioListTile(
+                value: "fargana",
+                activeColor: Colors.red,
+                selected: selectRegion == "fargana",
+                groupValue: selectRegion,
+                onChanged: (value) {
+                  selectRegion = value;
+                  setState(() {});
+                },
+                title: Text("Fargana"),
               ),
             ],
           ),
